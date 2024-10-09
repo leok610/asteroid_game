90e948741d41a646ba7a08f9ea77c00e12ae13fd
Leo Keliher <18314476+leok610@users.noreply.github.com>
2024-10-09 21:08:08 +0200

    added asteroid field

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from constants import *
 from circleshape import *
 from player import *
+from asteroidfield import *
 
 def main():
     # Initializes all submodules of pygame
@@ -14,15 +15,32 @@ def main():
     # Delta time variable
     dt = 0
 
-    # Spawn player
+    # create object groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    # Add player classe to object groups and spawn player
+    Player.containers = (updatable, drawable)
     player = Player(x = SCREEN_WIDTH /2, y = SCREEN_HEIGHT /2)
+
+
+    # Create asteroid object group and add asteroids
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    # Add the AsteroidField object to updatable group
+    AsteroidField.containers = (updatable)
+    field = AsteroidField()
+
     # Game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
+        for object in drawable:
+            object.draw(screen)
+        for object in updatable:
+            object.update(dt)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
